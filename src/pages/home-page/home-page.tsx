fb6a643ddc323a57c7bd882f493c4de485837a96
Jakub Löffelmann <jakubloffelmann@gmail.com>
2020-05-17 15:47:48 +0200

    Build test

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,13 +1,32 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import css from './home-page.scss'
+import { Input } from 'antd'
 
 const HomePage = () => {
+	const [sizeX, setSizeX] = useState(960)
+	const [sizeY, setSizeY] = useState(540)
+	console.log('a')
+
 	return (
 		<div
 			className={css.homePage}
 		>
-			HOME PAGE
+			<div>
+				<Input
+					value={sizeX}
+					addonBefore='Size X'
+					placeholder='Basic usage'
+					onChange={(e: any) => setSizeX(e.target.value)}
+				/>
+				<Input
+					value={sizeY}
+					addonBefore='Size Y'
+					placeholder='Basic usage'
+					onChange={(e: any) => setSizeY(e.target.value)}
+				/>
+			</div>
+			<hr />
 		</div>
 
 
